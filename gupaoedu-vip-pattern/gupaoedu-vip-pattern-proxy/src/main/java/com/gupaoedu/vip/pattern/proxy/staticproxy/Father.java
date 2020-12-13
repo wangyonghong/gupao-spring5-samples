4a1931b79ce9766d4465ce33e7cc2e36563bb65c
wangyonghong <yonghongwang@163.com>
2020-12-13 16:30:16 +0800

    :tada: update

--- gupaoedu-vip-pattern/gupaoedu-vip-pattern-proxy/src/main/java/com/gupaoedu/vip/pattern/proxy/staticproxy/Father.java
+++ gupaoedu-vip-pattern/gupaoedu-vip-pattern-proxy/src/main/java/com/gupaoedu/vip/pattern/proxy/staticproxy/Father.java
@@ -6,15 +6,15 @@ import com.gupaoedu.vip.pattern.proxy.Person;
  * Created by Tom on 2019/3/10.
  */
 public class Father implements Person {
-    private Son person;
+    private Son son;
 
-    public Father(Son person){
-        this.person = person;
+    public Father(Son son){
+        this.son = son;
     }
 
     public void findLove(){
         System.out.println("父亲物色对象");
-        this.person.findLove();
+        this.son.findLove();
         System.out.println("双方父母同意，确立关系");
     }
 
